0fa2f49664d11aa27c5875d1dd46fe0ea4d50422
Andrew Shen <andrewshenrocks@gmail.com>
2026-01-22 19:16:53 -0800

    Working on Part 1

--- lib/part1/part1.ml
+++ lib/part1/part1.ml
@@ -1,8 +1,17 @@
 let todo () = failwith "TODO"
-let singletons (xs : 'a list) : 'a list list = todo ()
-let map2d (f : 'a -> 'b) (xss : 'a list list) : 'b list list = todo ()
-let product (xs : 'a list) (ys : 'b list) : ('a * 'b) list list = todo ()
-let power (xs : 'a list) : 'a list list = todo ()
+
+let singletons (xs : 'a list) : 'a list list = List.map (fun x -> [x]) xs
+
+let map2d (f : 'a -> 'b) (xss : 'a list list) : 'b list list = List.map (List.map f) xss
+
+let product (xs : 'a list) (ys : 'b list) : ('a * 'b) list list =
+    List.map (fun x -> List.map (fun y -> (x, y)) ys) xs 
+
+let power (xs : 'a list) : 'a list list =
+    List.fold_right
+        (fun x acc -> acc @ List.map (fun subset -> x::subset) acc)
+        xs
+        [[]]
 
 let both : 'a option -> 'b option -> ('a * 'b) option =
- fun x -> match x with Some x -> todo () | None -> todo ()
+    fun x -> match x with Some x -> todo () | None -> todo ()
